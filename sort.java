fad406927a4be598f12c92fcab01b56632e39c41
VemavarapuManikanta <86473325+VemavarapuManikanta@users.noreply.github.com>
2021-06-25 22:02:38 +0530

    Add files via upload

---
+++ sort.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class  sort
+{
+	public static void main(String[] args) 
+	{
+		int a=2;
+		int b=3;
+		a=a+b;
+		b=a-b;
+		a=a-b;
+		System.out.println("a:"+a);
+System.out.println("b:"+b);
+
+	}
+}
